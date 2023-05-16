b00ff1d07ce727fb6425f7b0c253e6e55ef907e6
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-16 20:05:22 +0200

    add Wall.cs

---
+++ MazeGeneration/Assets/Scripts/Wall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Wall
+{
+
+    public Vector2Int Start { get; private set; }
+    public Vector2Int End { get; private set; }
+
+    public int Length => (int) Vector2Int.Distance(Start, End);
+    
+    public Wall(Vector2Int startPoint, Vector2Int endPoint)
+    {
+        Start = startPoint;
+        End = endPoint;
+    }
+
+    public override string ToString()
+    {
+        return Start + " " + End;
+    }
+}
