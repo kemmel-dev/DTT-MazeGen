8fef1dfa3c148e043a636efe1c83a7c820ff29fb
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-18 21:19:15 +0200

    add size randomizer to MazeConfigEditor

---
+++ MazeGeneration/Assets/Editor/MazeConfigEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(MazeConfig))]
+    public class MazeConfigEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var mazeConfig = (MazeConfig)target;
+
+            if (GUILayout.Button("Randomize size"))
+            {
+                mazeConfig.Size = new Vector2Int(
+                    Random.Range(mazeConfig.SizeMin.x, mazeConfig.SizeMax.x + 1),
+                    Random.Range(mazeConfig.SizeMin.y, mazeConfig.SizeMax.y + 1)
+                );
+
+            }
+        }
+    }
+}
