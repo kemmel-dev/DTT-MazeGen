3a7d60dcfd90eacd4f11d74f909668406707cb4c
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-20 14:58:31 +0200

    set ghostobject to null on play

--- MazeGeneration/Assets/Scripts/Maze/PlayMaze.cs
+++ MazeGeneration/Assets/Scripts/Maze/PlayMaze.cs
@@ -1,4 +1,5 @@
 using Character;
+using Ghost;
 using Maze.Builder;
 using Maze.ContentsEditor;
 using UnityEngine;
@@ -8,6 +9,7 @@ namespace Maze
     public class PlayMaze : MonoBehaviour
     {
 
+        [SerializeField] private GhostObject _ghostObject;
         [SerializeField] private Player _player;
         [SerializeField] private MazeBuilder _mazeBuilder;
         [SerializeField] private MazeContentsEditor _mazeContentsEditor;
@@ -16,6 +18,7 @@ namespace Maze
         {
             var start = _mazeContentsEditor.Start.position;
             var finish = _mazeContentsEditor.Finish.position;
+            _ghostObject.ChangeObject(null);
             _mazeBuilder.Build3DMaze();
             _player.InitializePlayer(start);
         }
