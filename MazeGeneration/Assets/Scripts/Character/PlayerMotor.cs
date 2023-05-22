d58b338ded3bc61837d6a5907d311298c796cc2e
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-22 11:30:44 +0200

    add comments to PlayerMotor

--- MazeGeneration/Assets/Scripts/Character/PlayerMotor.cs
+++ MazeGeneration/Assets/Scripts/Character/PlayerMotor.cs
@@ -6,11 +6,11 @@ namespace Character
     public class PlayerMotor : MonoBehaviour
     {
 
+        [Tooltip("Speed which the player moves at.")]
         [SerializeField] private float _speed = 50f;
         
         private Rigidbody _rigidbody;
         private Vector3 _moveInput;
-
 
         private void Awake()
         {
@@ -22,17 +22,24 @@ namespace Character
             Move();
         }
 
+        /// <summary>
+        /// Moves the player according to the move input.
+        /// </summary>
         private void Move()
         {
             var thisTransform = transform;
             var forward = thisTransform.forward.normalized * (_moveInput.z * Time.deltaTime);
             var right = thisTransform.right.normalized * (_moveInput.x * Time.deltaTime);
 
-            var combined = (forward + right) * _speed;
-            combined.y = _rigidbody.velocity.y;
-            _rigidbody.velocity = combined;
+            var force = (forward + right) * _speed;
+            force.y = _rigidbody.velocity.y;
+            _rigidbody.velocity = force;
         }
 
+        /// <summary>
+        /// Update the movement input. Should be called each frame.
+        /// </summary>
+        /// <param name="input">The movement input.</param>
         public void SetMovementInput(Vector3 input)
         {
             _moveInput = input;
