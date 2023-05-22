5eddd6f0c3213912ae484002f5aa32016a2883f4
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-22 11:23:56 +0200

    add comments to PanAndZoomCameraController

--- MazeGeneration/Assets/Scripts/CameraTools/PanAndZoomCameraController.cs
+++ MazeGeneration/Assets/Scripts/CameraTools/PanAndZoomCameraController.cs
@@ -5,7 +5,9 @@ namespace CameraTools
     public class PanAndZoomCameraController : MonoBehaviour
     {
 
+        [Tooltip("Sensitivity with which the camera pans when dragging.")]
         [SerializeField] private float _panSensitivity;
+        [Tooltip("Sensitivity with which the camera zooms when scrolling.")]
         [SerializeField] private float _zoomSensitivity;
 
         private Camera _camera;
@@ -16,12 +18,14 @@ namespace CameraTools
         }
 
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
+            // Zoom
             _camera.orthographicSize += - Input.mouseScrollDelta.y * _zoomSensitivity;
         
             if (!Input.GetMouseButton(1)) return;
         
+            // Pan
             var thisTransform = transform;
             var currentPos = thisTransform.position;
             var delta =  - new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * _panSensitivity;
