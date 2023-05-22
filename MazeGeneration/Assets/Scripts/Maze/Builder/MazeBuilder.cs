b3db4a3a1028329f6530cab7a8b2ddc7577cc14f
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-22 11:43:38 +0200

    add comments to MazeBuilder

--- MazeGeneration/Assets/Scripts/Maze/Builder/MazeBuilder.cs
+++ MazeGeneration/Assets/Scripts/Maze/Builder/MazeBuilder.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Maze.Generation;
 using UnityEngine;
 
 namespace Maze.Builder
@@ -10,27 +9,31 @@ namespace Maze.Builder
         private Wall[] _walls = Array.Empty<Wall>();
         private Transform _mazeParent;
         private Coroutine _buildRoutine;
-        private IMazeBuilder _mazeBuilder;
+        private IMazeBuilder _builderImplementation;
     
-        // Access these components only through their properties, so they work from EditMode.
-        private MazeConfig _mazeConfigBackingField;
+        // Do not access MazeConfig through it's backing field, or it won't be accessible from EditMode.
+        private MazeConfig b_mazeConfig;
 
         private MazeConfig MazeConfig
         {
             get
             {
-                if (_mazeConfigBackingField != null) return _mazeConfigBackingField;
-                _mazeConfigBackingField = GetComponent<MazeConfig>();
-                return _mazeConfigBackingField;
+                if (b_mazeConfig != null) return b_mazeConfig;
+                b_mazeConfig = GetComponent<MazeConfig>();
+                return b_mazeConfig;
             }
+            set => b_mazeConfig = value;
         }
         
     
         private void Awake()
         {
-            _mazeConfigBackingField = GetComponent<MazeConfig>();
+            MazeConfig = MazeConfig.Instance;
         }
 
+        /// <summary>
+        /// Ensures a clean Maze Parent object, destroying old contents if they exist. 
+        /// </summary>
         private void RefreshParent()
         {
             if (_mazeParent == null)
@@ -47,6 +50,12 @@ namespace Maze.Builder
             GenerateParent();
         }
 
+        /// <summary>
+        /// Places the maze content objects in the maze.
+        /// </summary>
+        /// <param name="startPos">The start position of the maze.</param>
+        /// <param name="endPos">The end position of the maze.</param>
+        /// <param name="keyPosition">The position at which the key should spawn.</param>
         public void PlaceObjects(Vector3 startPos, Vector3 endPos, Vector3 keyPosition)
         {
             Instantiate(MazeConfig.StartPrefab, startPos, Quaternion.identity, _mazeParent);
@@ -54,41 +63,63 @@ namespace Maze.Builder
             Instantiate(MazeConfig.KeyPrefab, keyPosition, Quaternion.identity, _mazeParent);
         }
 
+        /// <summary>
+        /// Generates a new maze parent object.
+        /// </summary>
         private void GenerateParent()
         {
             _mazeParent = new GameObject("Maze Parent").transform;
             _mazeParent.parent = transform;
         }
         
+        /// <summary>
+        /// Builds a 3D version of the maze.
+        /// </summary>
         public void Build3DMaze()
         {
             BuildMaze(MazeConfig.Size, true, true, false);
         }
 
+        /// <summary>
+        /// Builds a 2D version of the maze.
+        /// </summary>
+        /// <param name="instant">Whether to instantly build the maze (true) or use a coroutine (false).</param>
         public void Build2DMaze(bool instant)
         {
             BuildMaze(MazeConfig.Size, instant, false, true);
         }
 
+        /// <summary>
+        /// (Optionally generates and) builds a maze.
+        /// </summary>
+        /// <param name="vector2Int">Size of the maze to generate and build.</param>
+        /// <param name="instant">Whether to instantly build the maze (true) or use a coroutine (false).</param>
+        /// <param name="buildIn3D">Building for 2d (false) or 3d (true)?</param>
+        /// <param name="newMaze">Whether to generate a new maze or build an existing maze.</param>
         private void BuildMaze(Vector2Int vector2Int, bool instant, bool buildIn3D = false, bool newMaze = false)
         {
             RefreshParent();
 
-            _mazeBuilder =  buildIn3D ? new MazeBuilderMesh(_mazeParent, MazeConfig) : new MazeBuilderLines(_mazeParent, MazeConfig);
-            _mazeBuilder.BuildOuterWalls();
+            // Build walls
+            _builderImplementation =  buildIn3D ? new MazeBuilderMesh(_mazeParent, MazeConfig) : new MazeBuilderLines(_mazeParent, MazeConfig);
+            _builderImplementation.BuildOuterWalls();
 
+            // Stop build routine if it was running
             if (_buildRoutine != null)
                 StopCoroutine(_buildRoutine);
-        
+            
+            // Generate new maze if required
             if (newMaze)
                 _walls = MazeConfig.MazeGenerator.Generate(vector2Int);
+            
+            // Build maze instantly or by using a routine.
             if (instant)
             {
-                _mazeBuilder.BuildInnerWalls(_walls);
+                _builderImplementation.BuildInnerWalls(_walls);
             }
             else
             {
-                _buildRoutine = StartCoroutine(_mazeBuilder.BuildInnerWallsRoutine(_walls));
+                _buildRoutine = StartCoroutine(_builderImplementation.BuildInnerWallsRoutine(_walls));
             }
         }
     
