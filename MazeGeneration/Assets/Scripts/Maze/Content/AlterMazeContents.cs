9c9fa6d3e87903d9fc5e1f1c5a841dd751da5bad
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-22 11:54:14 +0200

    add comments to AlterMazeContents.cs

--- MazeGeneration/Assets/Scripts/Maze/Content/AlterMazeContents.cs
+++ MazeGeneration/Assets/Scripts/Maze/Content/AlterMazeContents.cs
@@ -16,9 +16,24 @@ namespace Maze.Content
         private Camera _cam;
         private MazeObject _currentObjectType = MazeObject.None;
         
+        /// <summary>
+        /// Transform of placed Start object. Null if not placed yet.
+        /// </summary>
         public Transform Start { get; private set; }
+        
+        /// <summary>
+        /// Transform of placed Finish object. Null if not placed yet.
+        /// </summary>
         public Transform Finish { get; private set; }
+        
+        /// <summary>
+        /// Transform of placed Key object. Null if not placed yet.
+        /// </summary>
         public Transform Key { get; private set; }
+        
+        /// <summary>
+        /// Returns whether an object is currently selected to be placed.
+        /// </summary>
         private bool ActiveSelection => _currentObjectType != MazeObject.None;
         private bool AllPlaced => Start != null && Finish != null && Key != null;
 
@@ -31,8 +46,7 @@ namespace Maze.Content
                 return worldPointUnderMouse;
             }
         }
-
-
+        
         private void Awake()
         {
             _cam = Camera.main;
@@ -55,6 +69,11 @@ namespace Maze.Content
             _ghostObject.SetPosition(WorldPointUnderMouse);
         }
 
+        /// <summary>
+        /// Place currently selected object.
+        /// </summary>
+        /// <exception cref="NullReferenceException">Thrown when function is called without there being a maze.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when MazeObject enum value exceeds range.</exception>
         private void PlaceSelection()
         {
             var mazeParent = _mazeConfig.MazeBuilder.transform.GetChild(0);
@@ -86,27 +105,47 @@ namespace Maze.Content
             _goButton.RefreshEnabled(AllPlaced);
         }
 
+        /// <summary>
+        /// Picks the start prefab to be placed.
+        /// Used by button events.
+        /// </summary>
         public void PickStart()
         {
             PickPrefab(MazeObject.Start, _mazeConfig.StartGhostPrefab);
         }
 
+        /// <summary>
+        /// Picks the finish prefab to be placed.
+        /// Used by button events.
+        /// </summary>
         public void PickFinish()
         {
             PickPrefab(MazeObject.Finish, _mazeConfig.FinishGhostPrefab);
         }
 
+        /// <summary>
+        /// Picks the key prefab to be placed.
+        /// Used by button events.
+        /// </summary>
         public void PickKey()
         {
             PickPrefab(MazeObject.Key, _mazeConfig.KeyGhostPrefab);
         }
 
+        /// <summary>
+        /// Picks a prefab to be placed.
+        /// </summary>
+        /// <param name="objectType">The enum value corresponding to the type of object</param>
+        /// <param name="prefab">The prefab that corresponds to the object</param>
         private void PickPrefab(MazeObject objectType, GameObject prefab) 
         {
             _currentObjectType = objectType;
             _ghostObject.ChangeObject(prefab);
         }
-
+        
+        /// <summary>
+        /// Clears the active selection 
+        /// </summary>
         private void ClearSelection()
         {
             _currentObjectType = MazeObject.None;
