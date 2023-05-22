bb0df6806820f265e8d56de20ee7403cdd63203c
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-22 11:26:00 +0200

    add comments for FirstPersonCamera

--- MazeGeneration/Assets/Scripts/Character/FirstPersonCamera.cs
+++ MazeGeneration/Assets/Scripts/Character/FirstPersonCamera.cs
@@ -15,13 +15,21 @@ namespace Character
             enabled = false;
         }
 
+        /// <summary>
+        /// Adjusts the player rotation and camera vertical angle based on the mouse input. 
+        /// </summary>
+        /// <param name="mouseInput">The mouse input this frame.</param>
         public void AdjustAngle(Vector2 mouseInput)
         {
+            // Amplify input by sensitivity
             var input = mouseInput * _sensitivity;
             var thisTransform = transform;
             
+            // Update the vertical angle
             _currentVerticalAngle = Mathf.Clamp(_currentVerticalAngle - input.y, -_verticalAngleLimit, _verticalAngleLimit);
             thisTransform.localEulerAngles = new Vector3(_currentVerticalAngle , 0, 0);
+            
+            // Update the player rotation (which this script should be parented under)
             var parent = thisTransform.parent;
             parent.eulerAngles = new Vector3(0, parent.eulerAngles.y + input.x, 0);
         }
