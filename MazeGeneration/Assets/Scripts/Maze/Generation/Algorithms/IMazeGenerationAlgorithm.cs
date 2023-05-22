ab243856a026f2ae43f4f4cace30b0634e9716fc
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-22 11:55:39 +0200

    add comments to IMazeGenerationAlgorithm

--- MazeGeneration/Assets/Scripts/Maze/Generation/Algorithms/IMazeGenerationAlgorithm.cs
+++ MazeGeneration/Assets/Scripts/Maze/Generation/Algorithms/IMazeGenerationAlgorithm.cs
@@ -4,6 +4,11 @@ namespace Maze.Generation.Algorithms
 {
     public interface IMazeGenerationAlgorithm
     {
+        /// <summary>
+        /// Generates the walls for a maze.
+        /// </summary>
+        /// <param name="mazeSize">The size the maze should be.</param>
+        /// <returns>The generated Wall objects representing the walls of the maze.</returns>
         public Wall[] GenerateWalls(Vector2Int mazeSize);
     }
 }
