138f943cfb6a49103e3fe627ca1865572e080b14
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-22 10:40:01 +0200

    refactor dependencies for PlayerMotor

--- MazeGeneration/Assets/Scripts/Character/PlayerMotor.cs
+++ MazeGeneration/Assets/Scripts/Character/PlayerMotor.cs
@@ -5,10 +5,12 @@ namespace Character
     [RequireComponent(typeof(Rigidbody))]
     public class PlayerMotor : MonoBehaviour
     {
+
+        [SerializeField] private float _speed = 50f;
+        
         private Rigidbody _rigidbody;
         private Vector3 _moveInput;
 
-        public float Speed;
 
         private void Awake()
         {
@@ -26,7 +28,7 @@ namespace Character
             var forward = thisTransform.forward.normalized * (_moveInput.z * Time.deltaTime);
             var right = thisTransform.right.normalized * (_moveInput.x * Time.deltaTime);
 
-            var combined = (forward + right) * Speed;
+            var combined = (forward + right) * _speed;
             combined.y = _rigidbody.velocity.y;
             _rigidbody.velocity = combined;
         }
