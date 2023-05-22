db39db053d562301ff4021b8d188e2366b7a09af
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-22 11:45:35 +0200

    add comments to IMazeBuilder

--- MazeGeneration/Assets/Scripts/Maze/Builder/IMazeBuilder.cs
+++ MazeGeneration/Assets/Scripts/Maze/Builder/IMazeBuilder.cs
@@ -4,8 +4,19 @@ namespace Maze.Builder
 {
     public interface IMazeBuilder
     {
+        /// <summary>
+        /// Builds the outer walls for the maze.
+        /// </summary>
         void BuildOuterWalls();
+        /// <summary>
+        /// Builds the inner walls for the maze with a delay in each step.
+        /// </summary>
+        /// <param name="walls">The inner walls to build.</param>
         IEnumerator BuildInnerWallsRoutine(Wall[] walls);
+        /// <summary>
+        /// Builds the inner walls for the maze instantly.
+        /// </summary>
+        /// <param name="walls">The inner walls to build.</param>
         void BuildInnerWalls(Wall[] walls);
     }
 }
