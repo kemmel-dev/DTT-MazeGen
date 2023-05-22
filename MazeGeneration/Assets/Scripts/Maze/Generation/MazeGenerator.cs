6f6e653e0363b489646c0223afbacebdaa655b2d
Kamiel de Visser <kamieldevisser@gmail.com>
2023-05-22 12:01:44 +0200

    add comments to MazeGenerator

--- MazeGeneration/Assets/Scripts/Maze/Generation/MazeGenerator.cs
+++ MazeGeneration/Assets/Scripts/Maze/Generation/MazeGenerator.cs
@@ -6,9 +6,15 @@ namespace Maze.Generation
 {
     public class MazeGenerator : MonoBehaviour
     {
-        [SerializeField]
-        private GenerationAlgorithm _algorithm = GenerationAlgorithm.RecursiveDivision;
+        [SerializeField] private GenerationAlgorithm _algorithm = GenerationAlgorithm.RecursiveDivision;
 
+        /// <summary>
+        /// Generate the walls for a maze.
+        /// </summary>
+        /// <param name="mazeSize">The size of the maze</param>
+        /// <returns>The walls of the maze.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the GenerationAlgorithm enum is set to a value
+        /// that exceeds it's range</exception>
         public Wall[] Generate(Vector2Int mazeSize)
         {
             return (_algorithm switch
